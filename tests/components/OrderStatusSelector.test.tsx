6bc1971ec3b844b1c1caafc55733533a642c7b70
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2024-04-20 10:38:13 +0545

    Add OrderStatusSelector test suite

---
+++ tests/components/OrderStatusSelector.test.tsx
@@ -0,0 +1,102 @@
+import { Theme } from '@radix-ui/themes';
+import { render, screen } from '@testing-library/react';
+import OrderStatusSelector from '../../src/components/OrderStatusSelector';
+import userEvent from '@testing-library/user-event';
+
+describe('OrderStatusSelector', () => {
+  const renderComponent = () => {
+    const onChange = vi.fn();
+
+    render(
+      <Theme>
+        <OrderStatusSelector onChange={onChange} />
+      </Theme>
+    );
+
+    const tigger = screen.getByRole('combobox');
+
+    return {
+      onChange,
+      tigger,
+      getOptions: () => screen.getAllByRole('option'),
+    };
+  };
+
+  it('should render the component with correct status', async () => {
+    const { tigger, getOptions } = renderComponent();
+    const user = userEvent.setup();
+
+    await user.click(tigger);
+
+    const options = getOptions();
+    expect(tigger).toHaveTextContent(/new/i);
+    expect(options).toHaveLength(3);
+    const labels = options.map(option => option.textContent);
+    expect(labels).toEqual(['New', 'Processed', 'Fulfilled']);
+  });
+
+  it('should call onChange when selecting an option', async () => {
+    const { onChange, tigger, getOptions } = renderComponent();
+    const user = userEvent.setup();
+
+    await user.click(tigger);
+
+    const options = getOptions();
+    await user.click(options[1]);
+
+    expect(onChange).toHaveBeenCalledWith('processed');
+  });
+
+  it("should not display options when it's closed", async () => {
+    const { tigger, getOptions } = renderComponent();
+
+    const initialOptions = screen.queryAllByRole('option');
+
+    expect(initialOptions).toHaveLength(0);
+
+    const user = userEvent.setup();
+    await user.click(tigger);
+
+    expect(getOptions()).toHaveLength(3);
+  });
+
+  it('should call onChange WithCorrect value', async () => {
+    const { onChange, tigger } = renderComponent();
+
+    const user = userEvent.setup();
+    await user.click(tigger);
+
+    const processedButton = screen.getByRole('option', {
+      name: /processed/i,
+    });
+
+    await user.click(processedButton);
+
+    expect(onChange).toHaveBeenCalledWith('processed');
+  });
+
+  it.each([
+    {
+      option: 'processed',
+    },
+    {
+      option: 'fulfilled',
+    },
+  ])('should call onChange with correct value when selecting %s', async ({ option }) => {
+    const { onChange, tigger } = renderComponent();
+
+    const user = userEvent.setup();
+    await user.click(tigger);
+
+    const optionButton = screen.getByRole('option', {
+      name: new RegExp(option, 'i'),
+      selected: false,
+    });
+
+    await user.click(optionButton);
+
+    expect(onChange).toHaveBeenCalledWith(option);
+
+    expect(tigger).toHaveTextContent(new RegExp(option, 'i'));
+  });
+});
