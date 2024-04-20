25a7764142da6382a78908d3765327de9fc33d31
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2024-04-20 15:08:24 +0545

    Disable '@typescript-eslint/no-unsafe-assignment' rule in ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unsafe-assignment': 'off',
   },
 };
