f0f712dbd2c1d4941e11199a78c9a6f7702cf162
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2024-04-30 22:37:01 +0545

    feat(): added useTimeoutFn and useDebounce hooks

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,13 @@
+import { DependencyList, useEffect } from 'react';
+import useTimeoutFn from './useTimeoutFn';
+
+export type UseDebounceReturn = [() => boolean | null, () => void];
+
+export default function useDebounce(fn: VoidFunction, ms: number = 0, deps: DependencyList = []): UseDebounceReturn {
+  const [isReady, cancel, reset] = useTimeoutFn(fn, ms);
+
+  //   eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(reset, deps);
+
+  return [isReady, cancel];
+}
